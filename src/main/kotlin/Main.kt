9401f44905d9d022f104e42852de500091bddd1d
Hongseob Lee <sjun2918@naver.com>
2024-04-09 17:34:19 +0900

    add 계산기1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
 package org.example
 
-fun main() {
-    println("Hello World!")
+import java.util.*
+
+fun main(array: Array<String>) = with(Scanner(System.`in`)) {
+    val calculator = Calculator()
+
+    println("숫자 두개 입력하세요.")
+    val num1 = nextInt()
+    val num2 = nextInt()
+
+    println("\n===결과===")
+    println("$num1 + $num2 = ${calculator.add(num1, num2)}")
+    println("$num1 - $num2 = ${calculator.minus(num1, num2)}")
 }
